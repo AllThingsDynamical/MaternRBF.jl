75c96e78b2bae4e7944acc31138ab5ff7ed385b2
Documenter.jl <documenter@juliadocs.github.io>
2025-06-26 13:05:44 +0000

    build based on 4ea35fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MaternRBF","page":"Home","title":"MaternRBF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MaternRBF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
